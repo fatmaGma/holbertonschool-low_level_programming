d6a25ca1140e9dcbbabcff61f974e26366abfc82
fatmaGma <8891@holbertonstudents.com>
2024-02-19 14:00:24 +0100

    creating the file 6-size.c and adding the answer

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,11 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i;
 	double d;
 	char c;
+
 	printf("the size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("the size of a double is; %lu.\n", (unsigned long)sizeof(d));
 	printf("the size of a c is; %lu.\n", (unsigned long)sizeof(d));
