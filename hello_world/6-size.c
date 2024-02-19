2e9cc64c115d6dd2259f660ef28d6933281418f7
fatmaGma <8891@holbertonstudents.com>
2024-02-19 13:59:13 +0100

    creating the file 6-size.c and adding the answer

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int i;
+	double d;
+	char c;
+	printf("the size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("the size of a double is; %lu.\n", (unsigned long)sizeof(d));
+	printf("the size of a c is; %lu.\n", (unsigned long)sizeof(d));
+	return (0);
+}
