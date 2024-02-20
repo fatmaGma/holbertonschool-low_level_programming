d5d7aa839bbcb92d5ef56f4fa3141fa9c49eb35b
fatmaGma <8891@holbertonstudents.com>
2024-02-20 14:23:48 +0100

    creation of the file and coding

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 10; i < 16; i++)
+	{
+		putchar(i + 'a' - 10);
+	}
+	putchar('\n');
+	return (0);
+}
