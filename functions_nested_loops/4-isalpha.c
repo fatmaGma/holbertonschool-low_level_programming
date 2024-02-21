a83e919b1df5d0c1cc5faa52c95c218ce59ade17
fatmaGma <8891@holbertonstudents.com>
2024-02-21 12:05:34 +0100

    creation of the file and adding the code

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int _isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int _putchar(char c);
 int print_alphabet(void);
 int print_alphabet_x10(void);
 int _islower(char c);
+int _isalpha(char c);
 
 #endif
