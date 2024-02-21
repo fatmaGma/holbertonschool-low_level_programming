82b3dcbaadba40534628aa54576e8049d4e6176e
fatmaGma <8891@holbertonstudents.com>
2024-02-21 10:45:54 +0100

    creation of the files and adding the code

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int print_alphabet_x10(void)
+{
+	char l;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
