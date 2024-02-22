76095342d1c5956de895bd2a40844b9e91fedc33
fatmaGma <8891@holbertonstudents.com>
2024-02-22 12:36:32 +0100

    creation of the files and coding

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,17 @@ int print_last_digit(int n)
 	int last_digit;
 	
 	last_digit = n % 10;
+	if (n >= 0)
+	{
+		last_digit += '0';
+		_putchar(last_digit);
+	}	
+	else
+	{
+		last_digit *= -1;
+                last_digit += '0';
+		_putchar(last_digit);
+
+	}
 	return (last_digit);
 }
