07d619f82215ef5a5ec0b0cfc3952b5168e70a2d
fatmaGma <8891@holbertonstudents.com>
2024-02-22 12:56:04 +0100

    fixing the code of the task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,12 @@ int print_last_digit(int n)
 	last_digit = n % 10;
 	if (n >= 0)
 	{
-		last_digit += '0';
-		_putchar(last_digit);
+		_putchar(last_digit + '0');
 	}	
 	else
 	{
-		last_digit *= -1;
-                last_digit += '0';
-		_putchar(last_digit);
+		last_digit = -last_digit;
+		_putchar(last_digit + '0');
 
 	}
 	return (last_digit);
