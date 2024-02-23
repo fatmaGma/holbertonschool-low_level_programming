ed5bd7858e7494a86b3c564b7705789d50053f94
fatmaGma <8891@holbertonstudents.com>
2024-02-23 11:07:57 +0100

    fixing the code

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,81 @@
 void print_to_98(int n)
 {
 	int i;
-	char buffer[12];
 
-	for (i = n; i < 99; i++)
+	if (n <= 98)
 	{
-		int temp = i;
-		int count = 0;
-		int j;
-
-		while (temp != 0)
+		for (i = n; i <= 98; i++)
 		{
-			temp /= 10;
-			count++;
+			if (i < 10 && i >= 0)
+			{
+				if (i != n)
+					_putchar(',');
+				_putchar(' ');
+				_putchar('0' + i);
+			}
+			else if (i < 0 && i > -10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('-');
+				_putchar('0' + (-i));
+			}
+			else
+			{
+				if (i != n && i != 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (i < 0)
+				{
+					_putchar('-');
+					_putchar('0' + ((-i) / 10));
+					_putchar('0' + ((-i) % 10));
+				}
+				else
+				{
+					_putchar('0' + (i  / 10));
+					_putchar('0' + (i % 10));
+				}
+			}
 		}
-		for (j = count - 1; j >= 0; j--)
-		{
-			buffer[j] = i % 10 + '0';
-			i /= 10;
-		}
-		for (j = 0; j < count; j++)
-			_putchar(buffer[j]);
-		if (i + 1 != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		i = temp;
-	}
 	_putchar('\n');
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i < 10 && i >= 0)
+			{
+				if (i != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + i);
+				}
+			}
+			else if (i > 10)
+			{
+				if (i != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (i < 100)
+				{
+					_putchar('0' + (i / 10));
+					_putchar('0' + (i % 10));
+				
+				}
+				else
+				{
+					_putchar('1');
+					_putchar('0' + ((i - 100) / 10));
+					_putchar('0' + ((i - 100) % 10));
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
