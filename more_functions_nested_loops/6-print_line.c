c408651fc99dcee414f8f55ef806613629969db7
fatmaGma <8891@holbertonstudents.com>
2024-02-26 14:13:01 +0100

    fixing the output

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n - 1; i++)
+	i = 1;
+	while (i <= n && n != 0)
 	{
-		if (i > 0)
-		{
-			_putchar('_');
-			if (i == n - 1)
-				_putchar('\n');
-		}
-		else 
+		if (n <= 0)
 			_putchar('\n');
+		else
+			_putchar('_');
+	i++;
 	}
+	_putchar('\n');
 }
