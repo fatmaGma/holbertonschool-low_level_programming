aa6082cff6ba1063e806a2cbb6efd587c3967fe2
fatmaGma <8891@holbertonstudents.com>
2024-02-26 12:00:58 +0100

    creatin of the files and coding

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Return: void.
  */
 void print_numbers(void)
 {
