37d202c28de0e33b0b6c8b11754eedabb9bac937
fatmaGma <8891@holbertonstudents.com>
2024-02-27 13:54:22 +0100

    creation of the files and coding

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *@s: char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	length = strlen(s);
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
