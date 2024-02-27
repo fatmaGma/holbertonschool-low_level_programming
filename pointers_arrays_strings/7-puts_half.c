89d89e7213f791629c6130e105ec4ffca957ddf7
fatmaGma <8891@holbertonstudents.com>
2024-02-27 15:05:29 +0100

    fixing the output

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,10 +15,9 @@ void puts_half(char *str)
 		len++;
 	}
 
-	for (i = len / 2; str[i] != '\0'; i++)
+	for (i = (len - 1) / 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
-
