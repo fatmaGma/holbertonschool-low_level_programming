38863aa0f86706ecafc1a635386a19829c070b28
fatmaGma <8891@holbertonstudents.com>
2024-02-27 13:57:24 +0100

    fixing betty

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,8 +10,13 @@ void print_rev(char *s)
 	int length;
 	int i;
 
-	length = strlen(s);
-	for (i = length - 1; i >= 0; i--)
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	for (i = length; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
