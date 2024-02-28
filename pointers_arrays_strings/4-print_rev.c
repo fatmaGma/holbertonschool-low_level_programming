163534d05cb1a5b411e310c708f569dfaeea9c47
fatmaGma <8891@holbertonstudents.com>
2024-02-28 11:25:14 +0100

    fixing the output

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@ void print_rev(char *s)
 	int i;
 
 	length = 0;
-	while (*s != '\0')
-	{
+	while (s[length] != '\0')
 		length++;
-		s++;
-	}
-	for (i = length - 1; i >= 0; i--)
+
+	for (i = length -1; i >= 0; i--)
+	{
 		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
