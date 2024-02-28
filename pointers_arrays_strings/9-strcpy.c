ae8f3613a125680871b20c9959cbc3599c64e01c
fatmaGma <8891@holbertonstudents.com>
2024-02-28 12:12:28 +0100

    creation the files and coding

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - ckeck the code
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *dest_start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return dest_start;
+}
