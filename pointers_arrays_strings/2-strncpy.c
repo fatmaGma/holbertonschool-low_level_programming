781f911d0fbbf158c8b21a381bdc960b8f60295f
fatmaGma <8891@holbertonstudents.com>
2024-02-29 11:11:41 +0100

    creation of the files and coding

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncpy - function that copies a string.
+ * @dest: char
+ * @src: char
+ * @n: integer
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*p++ = *src++;
+		n--;
+	}
+	*p = '\0';
+	return (dest);
+}
