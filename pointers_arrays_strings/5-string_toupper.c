99051629391e4791ac8a6b9eb4cbda5606bea334
fatmaGma <8891@holbertonstudents.com>
2024-03-01 10:07:56 +0100

    creation of the files and coding

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: char
+ * Return: char
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -= 32;
+		}
+		ptr++;
+	}
+	return (str);
+}
