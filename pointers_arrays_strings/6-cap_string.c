09f4060fae54538dae12f301e934af570ae6ecce
fatmaGma <8891@holbertonstudents.com>
2024-03-01 10:44:41 +0100

    creation of the files and coding

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: char
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int x = 1;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (x == 1 && isalpha(s[i]))
+		{
+			s[i] = toupper(s[i]);
+			x = 0;
+		}
+		else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+				s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+				s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+				s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+				s[i] == '}')
+			x = 1;
+	}
+	return (s);
+}
