c44d669a3000930b19a6d8d42ee91c17455fc7e5
fatmaGma <8891@holbertonstudents.com>
2024-03-01 09:05:09 +0100

    fixing betty

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reverse_array - reverses the content of an array of integers
+ * @a: integer
+ * @n: integer
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
 	int temp;
 	int i;
 
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
