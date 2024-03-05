07ad4e9452ebb5a84950d0c257fe2f437789fa37
fatmaGma <8891@holbertonstudents.com>
2024-03-05 17:23:55 +0100

    creation of the repository, directory and file and coding

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion -  prints a string, followed by a new line.
+ * @s: char
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+		return;
+	}
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+}
