890401ee4395264917a0fbc2011fe24789c9a9b7
fatmaGma <8891@holbertonstudents.com>
2024-03-07 11:01:11 +0100

    creation of the files and coding

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * is_prime_number - check the code
+ * @n: integer
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+		return (0);
+	for (i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
