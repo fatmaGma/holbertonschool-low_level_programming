858c9c44ff88848ef1166b2f0955833a6f58218e
fatmaGma <8891@holbertonstudents.com>
2024-03-07 12:18:18 +0100

    creation of the file and coding

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argv: string
  * Return: integer
  * **/
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
