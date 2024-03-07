d2bed50831f552d7ee32469ec75cc74559d2701e
fatmaGma <8891@holbertonstudents.com>
2024-03-07 12:41:31 +0100

    fixing betty

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,18 +11,19 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 1;
 
-	if (argc <= 1)
+	if (argc < 2)
 	{
-	       printf("Error");
-	       printf ("\n");
-	       return (1);
+		printf("Error");
+		printf("\n");
+		return (1);
 	}
 	else
 	{
 		for (i = 1; i < argc; i++)
 			sum *= atoi(argv[i]);
 		printf("%d", sum);
+		printf("\n");
+		return (0);
+
 	}
-	printf("\n");
-	return (0);
 }
