5061494604b8cea21b60f5cbfd2e818295f946ba
fatmaGma <8891@holbertonstudents.com>
2024-03-07 12:14:00 +0100

    creation of the file and codinf

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: integer
+ * @argv: string
+ * Return: integer
+ * **/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
