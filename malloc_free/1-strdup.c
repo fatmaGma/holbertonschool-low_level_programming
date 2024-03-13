f7bb0b1cfeb697c8deedf30b0ad9eab2faee4b45
fatmaGma <8891@holbertonstudents.com>
2024-03-13 12:58:31 +0100

    adjusting the code

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * *_strdup - that returns a pointer to a newly allocated space in memory
  * @str: pointer of char
