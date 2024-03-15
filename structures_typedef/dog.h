e295cfe7c19369b166af33edbbfcf2a23a00f610
fatmaGma <8891@holbertonstudents.com>
2024-03-15 12:33:51 +0100

    creation of the file, coding

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,13 +6,14 @@
  * @age: float
  * @owner: char
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif /* DOG_H */
