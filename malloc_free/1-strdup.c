78d205e058d0e6c0cea49f1f78c52cb357b7f241
fatmaGma <8891@holbertonstudents.com>
2024-03-15 11:59:21 +0100

    update

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,27 +1,33 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
- * *_strdup - that returns a pointer to a newly allocated space in memory
- * @str: pointer of char
- * Return: char
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str: string
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string.
  */
 char *_strdup(char *str)
 {
 	char *duplicate;
-	size_t len;
-	size_t i;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	len = strlen(str);
-	duplicate = (char *)malloc((strlen(str) + 1) * sizeof(char));
+
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
 	if (duplicate == NULL)
 		return (NULL);
-	
-	for (i = 0; i < len; i++)
-		duplicate[i] = str[i];
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
 	duplicate[len] = '\0';
 
 	return (duplicate);
