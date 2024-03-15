04af812f5d5660b84db608610554e5d8ee6ae522
fatmaGma <8891@holbertonstudents.com>
2024-03-15 12:21:29 +0100

    fixing betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - check the code
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
