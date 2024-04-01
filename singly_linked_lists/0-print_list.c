9e9b9efb33847d3f18c2f657b02db4d64d3bcebb
fatmaGma <fatmaagmati@gmail.com>
2024-04-01 12:01:00 +0000

    creation of the directory, the files and coding

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - check the code
+ * @list_t: 
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h) {
+	const list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL) {
+		printf("[%d] %s\n", current->len, current->str ? current->str : "(nil)");
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,11 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+typedef struct list_s {
+	char *str;
+	int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+
+#endif
