825114aaa7ab443a6b439d225931de41218615cc
fatmaGma <fatmaagmati@gmail.com>
2024-04-02 11:18:02 +0000

    creation of the files and coding

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list.
+ * @head: list_t
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}
